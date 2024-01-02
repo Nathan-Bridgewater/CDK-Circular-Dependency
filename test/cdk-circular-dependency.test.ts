e34108b7c3d664240a983ca88c0ffc9d34ebc6d9
Nathan-Bridgewater <nathanbridgey99@gmail.com>
2024-01-02 09:55:03 +0000

    Initial commit

---
+++ test/cdk-circular-dependency.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCircularDependency from '../lib/cdk-circular-dependency-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-circular-dependency-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCircularDependency.CdkCircularDependencyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
